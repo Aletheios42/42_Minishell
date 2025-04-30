79c34c0f0caaf2fa476ea89e0899a3826f58a5c7
homepc <elorentec@gmail.com>
2025-04-30 17:20:14 +0200

    comienzo built ins

---
+++ Src/echo.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elorente <elorente@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 17:11:19 by elorente          #+#    #+#             */
+/*   Updated: 2025/04/30 17:11:19 by elorente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Inc/minishell.h"
+
+int	ft_echo(char **args)
+{
+	int	i;
+	int	newline;
+
+	i = 1;
+	newline = 1;
+	if (args[i] && ft_strcmp(args[i], "-n") == 0)
+	{
+		newline = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_printf("%s", args[i]);
+		if (args[i + 1])
+			ft_printf(" ");
+		i++;
+	}
+	if (newline)
+		ft_printf("\n");
+	return (0);
+}
