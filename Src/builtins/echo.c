8f29643071a276606e6d7df857a6ecb705439384
Eddy Alejandro Lorente Corea <elorente@c1r12s5.42madrid.com>
2025-06-12 17:32:31 +0200

    error

--- Src/builtins/echo.c
+++ Src/builtins/echo.c
@@ -6,24 +6,37 @@
 /*   By: elorente <elorente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 17:11:19 by elorente          #+#    #+#             */
-/*   Updated: 2025/06/09 17:43:42 by elorente         ###   ########.fr       */
+/*   Updated: 2025/06/12 17:29:53 by elorente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Inc/minishell.h"
+#include <stdbool.h>
+
+int	is_n_flag(const char *str)
+{
+	int	i;
+
+	if (str[0] != '-')
+		return (0);
+	i = 1;
+	while (str[i] == 'n')
+		i++;
+	return (str[i] == '\0');
+}
 
 int	ft_echo(char *args[])
 {
-	int	i;
-	int	add_newline;
+	int		i;
+	bool	add_newline;
 
 	if (!args)
 		return (1);
 	i = 1;
-	add_newline = 1;
-	if (args[i] && ft_strcmp(args[i], "-n") == 0)
+	add_newline = true;
+	while (args[i] && is_n_flag(args[i]))
 	{
-		add_newline = 0;
+		add_newline = false;
 		i++;
 	}
 	while (args[i])
